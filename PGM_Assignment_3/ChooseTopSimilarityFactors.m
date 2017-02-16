5ba6f0585b1aaaf2c9958bed9c42ec218733bf18
TracyCuiC <can.cui@mail.mcgill.ca>
2017-02-15 23:04:14 -0500

    Matlab code.

---
+++ PGM_Assignment_3/ChooseTopSimilarityFactors.m
@@ -0,0 +1,37 @@
+function factors = ChooseTopSimilarityFactors (allFactors, F)
+% This function chooses the similarity factors with the highest similarity
+% out of all the possibilities.
+%
+% Input:
+%   allFactors: An array of all the similarity factors.
+%   F: The number of factors to select.
+%
+% Output:
+%   factors: The F factors out of allFactors for which the similarity score
+%     is highest.
+%
+% Hint: Recall that the similarity score for two images will be in every
+%   factor table entry (for those two images' factor) where they are
+%   assigned the same character value.
+%
+% Copyright (C) Daphne Koller, Stanford University, 2012
+
+% If there are fewer than F factors total, just return all of them.
+if (length(allFactors) <= F)
+    factors = allFactors;
+    return;
+end
+
+% Your code here:
+factors = repmat(struct('var', [], 'card', [], 'val', []), F, 1);
+score = zeros(length(allFactors), 1);
+for i=1:length(allFactors)
+    score(i)=sum(allFactors(i).val);
+end
+[val,index]=sort(score,'descend');
+for i=1:F
+    factors(i)=allFactors(index(i));
+end
+
+end
+
